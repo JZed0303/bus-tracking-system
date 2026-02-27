0823ee66bde5fcfa46f34de2a9661e7ddf46b956
Kyllercodes02 <kyllescabus5@gmail.com>
2026-02-27 16:37:47 +0800

    Changing ico

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8" />
-    <title>@yield('title') | Tocly - Admin & Dashboard Template</title>
+    <title>@yield('title')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
     <meta content="Themesdesign" name="author" />
@@ -36,7 +36,7 @@
     </script>
 
     <!-- App favicon -->
-    <link rel="shortcut icon" href="{{ URL::asset('build/images/favicon.ico') }}">
+    <link rel="shortcut icon" href="{{ URL::asset('build/images/logo-header.ico') }}">
 
     {{-- Vite (React + assets) --}}
     @viteReactRefresh
